c2624a7031aa4b9c9eeedaeb2849e14cd68f39f0
gaba <gaba.bouliva@gmail.com>
2025-08-22 22:02:44 +0200

    update schema and migrations

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,11 +1,16 @@
 -- name: CreateUser :one
-INSERT INTO users (id, created_at, updated_at, email)
+INSERT INTO users (id, created_at, updated_at, email, hashed_password)
 VALUES (
-    $1, NOW(), NOW(), $2
+    $1, NOW(), NOW(), $2, $3
 )
 RETURNING *;
 
--- name: GetUser :one
+-- name: GetUserById :one
 SELECT id, created_at, updated_at, email 
 FROM users
 WHERE id = $1;
+
+-- name: GetUserByEmail :one
+SELECT id, created_at, updated_at, email, hashed_password
+FROM users
+WHERE email = $1;
--- sql/schema/00001_users.sql
+++ sql/schema/00001_users.sql
@@ -4,7 +4,8 @@ CREATE TABLE users (
     id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    email TEXT UNIQUE NOT NULL
+    email TEXT UNIQUE NOT NULL,
+    hashed_password TEXT NOT NULL DEFAULT 'unset'
 );
 -- +goose StatementEnd
 
